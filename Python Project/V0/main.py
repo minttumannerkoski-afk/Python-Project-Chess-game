c8c1460d47fa5a879344c1979c90002b5c4e35e9
minttumannerkoski-afk <minttu.mannerkoski@gmail.com>
2025-12-27 16:39:41 +0100

    Updated1 main.py

--- Python Project/V0/main.py
+++ Python Project/V0/main.py
@@ -1,3 +1,10 @@
+"""CLI entry point for the chess project (v0).
+
+Allows users to move pieces by entering coordinates like: e2 e4.
+No chess-rule validation is performed in v0.
+"""
+
+
 from board import Board
 from rook import Rook
 
